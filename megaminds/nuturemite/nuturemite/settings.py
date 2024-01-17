5b6d5e005c06485472d47b246e5ef46647858b31
Muniraj C <munirajmechanical15@gmail.com>
2024-01-17 18:56:45 +0530

    first commit

--- megaminds/nuturemite/nuturemite/settings.py
+++ megaminds/nuturemite/nuturemite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-!xqk1&%5vi!f=cduwp%$w4l&4e04mwv_b8&@=c3jfip$+tgfmn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'now.sh']
+ALLOWED_HOSTS = ['megamindstask.vercel.app', 'nuturemite.com']
 
 
 # Application definition
